1d1341e1edc4e0bf00c602c1c30e05260bdaa44c
Larajjj <42802765+Larajjj@users.noreply.github.com>
2020-08-02 17:45:05 +0530

    Update keypoints.js

--- keypoints.js
+++ keypoints.js
@@ -100,3 +100,37 @@ function alt_beep(){
    putBeeper();
    
 }
+
+
+//PARAMETERS AND ARGUMENTS
+function getMilk(money) {   
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+
+  var numOfBottles = money/1.5
+
+  console.log("buy "+ Math.floor(numOfBottles)+ " bottles of milk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk(10);
+
+
+
+
+
