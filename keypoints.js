02a3852ce6bc190f7df088ce3958a605f4b37ccc
Larajjj <42802765+Larajjj@users.noreply.github.com>
2020-08-02 07:27:20 +0530

    Update keypoints.js

--- keypoints.js
+++ keypoints.js
@@ -2,7 +2,14 @@ Some useful websites:
 JS documantation:  https://developer.mozilla.org/en-US/docs/Web/JavaScript
 Principles of Writing Consistent, Idiomatic JavaScript:  https://github.com/rwaldron/idiomatic.js
 
-
+//STRING CONCATENATION 
 var message="Hello";
 var name="John";
 alert(message+" there"+", "+ name+".");
+
+
+//STRING LENGTHS & RETRIEVING THE NUMBER OF CHARACTERS 
+var tweet = prompt("Compose your tweet:");
+var tweetCount = tweet.length
+alert("You've written "+tweetCount+" characters, you have "+(140-tweetCount)+"characters remaining.")
+
