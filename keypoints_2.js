14a2f706a22262a31773f434d8ef532d4d11c65f
Larajjj <42802765+Larajjj@users.noreply.github.com>
2020-10-19 19:50:11 +0530

    Update keypoints_2.js

--- keypoints_2.js
+++ keypoints_2.js
@@ -4,3 +4,18 @@ prompt("What is their name?");
 var lovescore = Math.random() * 100;
 lovescore = Math.floor(lovescore) + 1;
 alert("Your love score is " + lovescore + "%");
+
+
+
+prompt("What is your name?");
+prompt("What is their name?");
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1;
+
+if (lovescore > 70){
+  alert("Your love score is " + lovescore + "%" + " You're made for each other:)");
+}
+else{
+  alert("Your love score is " + lovescore + "%");
+}
+
