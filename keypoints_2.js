50b49acd0ca7bcb36811ae6a23e4c2c0a6a28248
Larajjj <42802765+Larajjj@users.noreply.github.com>
2020-10-19 19:32:26 +0530

    Create keypoints_2.js

---
+++ keypoints_2.js
@@ -0,0 +1,6 @@
+//Love calculator
+prompt("What is your name?");
+prompt("What is their name?");
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore) + 1;
+alert("Your love score is " + lovescore + "%");
